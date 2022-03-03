1ea001b379c175abde277aff9b1c281ec7c8f31a
Simon <gevhaz@tutanota.com>
2022-03-04 00:00:18 +0100

    Add flag parsing and article download ability
    A "--file" flag is added, which takes the path to a file containing
    URLs. The content of the file is read, and used as URLs to fetch content
    from with Readability.

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
 import (
+    "flag"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    "time"
+
     readability "github.com/go-shiori/go-readability"
 )
 
 func main() {
+    var urls_file_path string
+    flag.StringVar(&urls_file_path, "file", "", "Path to file with URLs to articles to be fetched and converted")
+    flag.Parse()
+
+    url_data, err := os.ReadFile(urls_file_path)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    urls := strings.Split(strings.Trim(string(url_data), "\n"), "\n")
+
+    for i, url := range urls {
+        fmt.Printf("Processing URL number %01d: %s\n", i+1, url)
+
+        article, err := readability.FromURL(url, 30 * time.Second)
+        if err != nil {
+            log.Fatalf("Failed to parse url: %s\n%v\n", url, err)
+        }
+
+        fmt.Println(article.Title)
+    }
 }
